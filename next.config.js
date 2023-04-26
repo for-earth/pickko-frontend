63e8e904bab674c17c020b28a0f0a4813310de52
Harang <dbd02169@naver.com>
2023-04-27 01:48:47 +0900

    chore(storybook): 스토리북을 세팅하라 (#9)
    - 스토리북 세팅
    - 관련 dependency 설치
    - next config, storyboo config의 @svgr/webpack 옵션 수정
    - storybook eslint rule 추가
    - update next.js dependency -> 13.3.1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  compiler: {
+    reactRemoveProperties: isProd && {
+      properties: ['^data-test'],
+    },
+    removeConsole: isProd,
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -19,7 +27,17 @@ const nextConfig = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: [{
+              name: 'removeViewBox',
+              active: false,
+            }],
+          },
+        },
+      }],
     });
 
     return config;
