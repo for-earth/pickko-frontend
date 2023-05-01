9b0fb3f34c7d2188073cb8e7f681b5e3d45e971e
saseungmin <dbd02169@naver.com>
2023-05-01 17:09:47 +0900

    feat(Button): 공통으로 사용되는 버튼을 구현하라
    - 공통으로 사용되는 버튼을 구현
    - 스토리북에 정의

---
+++ lib/types/index.ts
@@ -0,0 +1,13 @@
+export type ColorType =
+  | 'black'
+  | 'white'
+  | 'lemon'
+  | 'salmon'
+  | 'berrymilk'
+  | 'cloud'
+  | 'moss'
+  | 'mustard'
+  | 'forest'
+  | 'indigo'
+  | 'yam'
+  | 'brick';
