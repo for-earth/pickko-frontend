bbdeb0c9d9c49637f7fee075f5fab4f42d9f3f44
Harang <dbd02169@naver.com>
2023-05-05 17:28:18 +0900

    style: 사용하는 아이콘을 추가하라 (#23)
    - 모바일에서 ative 액션 스타일 수정
    - 사용되는 아이콘 스토리북에 정의

--- lib/assets/icons/index.ts
+++ lib/assets/icons/index.ts
@@ -1,34 +1,52 @@
+import OutlineAlert from './outline/alert.svg';
+import ArrowDown from './outline/arrow-down.svg';
+import ArrowLeft from './outline/arrow-left.svg';
 import ArrowRight from './outline/arrow-right.svg';
+import ArrowUp from './outline/arrow-up.svg';
+import OutlineCheck from './outline/check.svg';
 import ChevronDown from './outline/chevron-down.svg';
+import ChevronLeft from './outline/chevron-left.svg';
 import OutlineFeed from './outline/feed.svg';
 import OutlineHeart from './outline/heart.svg';
-import OutlinePick from './outline/pick.svg';
+import Logout from './outline/logout.svg';
 import Refresh from './outline/refresh.svg';
 import Search from './outline/search.svg';
+import OutlineSetting from './outline/setting.svg';
 import OutlineUser from './outline/user.svg';
 import Write from './outline/write.svg';
+import SolidAlert from './solid/alert.svg';
+import SolidCheck from './solid/check.svg';
 import SolidFeed from './solid/feed.svg';
 import SolidHeart from './solid/heart.svg';
-import SolidPick from './solid/pick.svg';
+import SolidSetting from './solid/setting.svg';
 import SolidUser from './solid/user.svg';
 
 const OutlineIcon = {
-  ArrowRight,
-  ChevronDown,
   Feed: OutlineFeed,
-  Pick: OutlinePick,
+  Check: OutlineCheck,
+  User: OutlineUser,
   Heart: OutlineHeart,
+  Alert: OutlineAlert,
+  Setting: OutlineSetting,
+  ChevronDown,
+  ChevronLeft,
+  ArrowDown,
+  ArrowLeft,
+  ArrowUp,
+  ArrowRight,
   Refresh,
   Search,
-  User: OutlineUser,
+  Logout,
   Write,
 };
 
 const SolidIcon = {
   Feed: SolidFeed,
-  Pick: SolidPick,
+  Check: SolidCheck,
   User: SolidUser,
   Heart: SolidHeart,
+  Alert: SolidAlert,
+  Setting: SolidSetting,
 };
 
 export {
