776ab0ed8fecac4d7068945bab753de3619381ff
saseungmin <dbd02169@naver.com>
2023-05-08 22:53:54 +0900

    feat(Label): label 공통 컴포넌트 구현 및 스토리북 적용

--- components/common/Button/index.tsx
+++ components/common/Button/index.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import {
-  HTMLProps, ReactElement, ReactNode,
-} from 'react';
+import { HTMLProps, ReactElement, ReactNode } from 'react';
 
 import Link from 'next/link';
 
---
+++ components/common/Label/Label.stories.tsx
@@ -0,0 +1,82 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Label from '.';
+
+const meta: Meta<typeof Label> = {
+  title: 'Components/Label',
+  component: Label,
+  tags: ['autodocs'],
+  args: {
+    children: 'Label',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Label>;
+
+export const WhiteLabel: Story = {
+  args: {
+    color: 'white',
+  },
+};
+
+export const LemonLabel: Story = {
+  args: {
+    color: 'lemon',
+  },
+};
+
+export const SalmonLabel: Story = {
+  args: {
+    color: 'salmon',
+  },
+};
+
+export const BerrymilkLabel: Story = {
+  args: {
+    color: 'berrymilk',
+  },
+};
+
+export const CloudLabel: Story = {
+  args: {
+    color: 'cloud',
+  },
+};
+
+export const MossLabel: Story = {
+  args: {
+    color: 'moss',
+  },
+};
+
+export const MustardLabel: Story = {
+  args: {
+    color: 'mustard',
+  },
+};
+
+export const ForestLabel: Story = {
+  args: {
+    color: 'forest',
+  },
+};
+
+export const IndigoLabel: Story = {
+  args: {
+    color: 'indigo',
+  },
+};
+
+export const YamLabel: Story = {
+  args: {
+    color: 'yam',
+  },
+};
+
+export const BrickLabel: Story = {
+  args: {
+    color: 'brick',
+  },
+};
