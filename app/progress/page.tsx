b8ff3739657e219f40df0b0d8f48295f42fcbcb1
saseungmin <dbd02169@naver.com>
2023-05-24 23:49:49 +0900

    fear(progress): progress bar 구현

---
+++ app/progress/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Progress from 'components/common/Progress';
+
+export default function progress() {
+  return (
+    <div>
+      <Progress value={5} />
+    </div>
+  );
+}
