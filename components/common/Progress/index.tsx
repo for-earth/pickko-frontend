40d4b28e8f634aad6a35973d1d2bca5f7a8a8d10
Harang <dbd02169@naver.com>
2023-05-25 23:54:59 +0900

    feat: progress bar (#45)

--- components/common/Progress/index.tsx
+++ components/common/Progress/index.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { CSSProperties } from 'react';
+
+import styles from './index.module.scss';
+
 type Props = {
   value: number;
 };
@@ -5,15 +11,28 @@ type Props = {
 const markers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function Progress({ value }: Props) {
+  const dividerStyle = {
+    position: 'absolute',
+    height: '10px',
+    borderRight: '1px solid black',
+    pointerEvents: 'none',
+    zIndex: 10,
+    top: '4px',
+  } as CSSProperties;
+
+  const rangeDividers = [];
+
+  for (let i = 1 + 1; i < 10; i += 1) {
+    const leftPosition = `${((i - 1) / (10 - 1)) * 100}%`;
+    const divider = <div key={i} style={{ ...dividerStyle, left: leftPosition }} />;
+    rangeDividers.push(divider);
+  }
+
   return (
-    <>
-      <input type="range" name="volume" min={1} max={10} list="markers" defaultValue={value} disabled />
-      <datalist id="markers">
-        {markers.map((marker) => (
-          <option key={marker} value={marker} />
-        ))}
-      </datalist>
-    </>
+    <div className={styles.progressWrapper}>
+      {rangeDividers}
+      <input className={styles.progress} type="range" min={1} max={10} list="markers" defaultValue={value} />
+    </div>
   );
 }
 
